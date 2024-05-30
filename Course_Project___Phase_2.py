44570d1c2695e6e1c0c67ee4718e886fa1920e0b
username <useremail>
2024-05-30 14:54:28 -0700

    Phase 4

--- Course_Project___Phase_2.py
+++ Course_Project___Phase_2.py
@@ -1,9 +1,71 @@
 #Jessica Arellano#
-#CIS261 - Course Project Phase 3#
+#CIS261 - Course Project Phase 4#
 
-from xml.dom.minicompat import EmptyNodeList
+from datetime import datetime
 
+def CreateUsers():
+    print('Create users, passwords, and roles')
+    UserFile = open("Users.txt", "a+")
+    while True:
+        username = GetUserName()
+        if (username.upper() == "END"):
+            break
+        userpwd = GetUserPassword()
+        userrole = GetUserRole()
+        
+        UserDetail = username + "|" + userpwd + "|" + userrole + "\n"
+        UserFile.write(UserDetail)
+        
+    UserFile.close()
+    printuserinfo()
+    
+def GetUserName():
+    username = input("Enter user name or 'End' to quit: ")
+    return username
 
+def GetUserPassword():
+    pwd = input("Enter password: ")
+    return pwd
+
+def GetUserRole():
+    userrole = input("Enter role (Admin or User): ")
+    while True:
+        if (userrole.upper() == "ADMIN" or userrole.upper() == "USER"):
+            return userrole
+        else:
+            userrole = input("Enter role (Admin or User): ")
+            
+def printuserinfor():
+    UserFile = open("Users.txt", "r")
+    while True:
+        UserDetail = UserFile.readline()
+        if not UserDetail:
+            break
+        UserDetail = UserDetail.replace("\n", "")
+        UserList = UserDetail.split("|")
+        username = UserList[0]
+        userpassword = UserList[1]
+        userrole = UserList[2]
+        print("User Name: ", username, "Password: ", userpassword, "Role: ", userrole)
+        
+def Login():
+    UserFile = open("Users.txt", "r")
+    UserList = []
+    UserName = input("Enter User Name: ")
+    UserPwd = input("Enter Password: ")
+    UserRole = "None"
+    while True:
+        UserDetail = UserFile.readline()
+        if not UserDetail:
+            return UserRole, UserName, UserPwd
+        UserDetail = UserDetail.replace("\n", "")
+        
+        UserList = UserDetail.split("|")
+        if UserName == UserList[0] and UserPwd == UserList[1]:
+            UserRole = UserList[2] 
+            return UserRole, UserName
+    return UserRole, UserName
+    
 def getDatesWorked():
     fromDate = input("Please enter start date in the following format MM/DD/YYYY: ")
     endDate = input("Please enter end date in the following format MM/DD/YYYY: ")
@@ -39,8 +101,31 @@ def printInfo(empDetailList):
     totalGrossPay = 0.00
     totalTax = 0.00
     totalNetPay = 0.00
-    for empList in empDetailList:
+    EmpFile = open("Employees.txt", "r")
+    while True:
+        rundate = input("Enter start date for report (MM/DD/YYYY) or All for all data in file: ")
+        if (rundate.upper() == "ALL"):
+            break
+        try:
+            rundate = datetime.strptime(rundate, "%m/%d/%Y")
+            break
+        except ValueError:
+            print("Invalid date format. Try again.")
+            print()
+            continue
+    while True:
+        EmpDetail = EmpFile.readline()
+        if not EmpDetail:
+            break
+        EmpDetail = EmpDetailList.replace("\n", "")
+        
+        EmpList = EmpDetail.split("|")
         fromDate = empList[0]
+        if (str(rundate.upper() != "ALL"):
+            checkdate = datetime.strptime(fromdate, "%m/%d/%Y")
+            if (checkdate < rundate):
+                continue
+        todate = empList[1]
         empName = empList[2]
         hours = empList[3]
         hourlyRate = empList[4]
